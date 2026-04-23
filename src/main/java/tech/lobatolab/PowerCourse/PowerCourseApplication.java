f86e075f1728817c667deea986863257ea5107cb
Luis Eduardo Lobato <luislobato.medinaeduardo@gmail.com>
2026-04-22 18:34:16 -0600

    git init

---
+++ src/main/java/tech/lobatolab/PowerCourse/PowerCourseApplication.java
@@ -0,0 +1,13 @@
+package tech.lobatolab.PowerCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/lobatolab/PowerCourse/PowerCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.lobatolab.PowerCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
